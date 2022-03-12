a4068f96ef019386aac8be1bf43f8916a7a8faff
Maxwell Carlson <bart4282819@gmail.com>
2022-03-12 15:49:15 -0800

    Got feature vectors working I think

--- Clustering/Clustering.py
+++ Clustering/Clustering.py
@@ -24,7 +24,7 @@ batchSize = 128
 trainset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 
 indicies = []
-imagesPerCatagory = 100
+imagesPerCatagory = 1000
 catagoriesCount = {x : 0 for x in range(10)}
 for i in range(len(trainset)):
     if catagoriesCount[trainset[i][1]] < imagesPerCatagory:
@@ -40,31 +40,99 @@ for i in range(len(trainset)):
     if filledCatagories == 10:
         break
 
-batchSize=imagesPerCatagory*10
+batchSize = 128
+subsetSize = imagesPerCatagory*10
 subset = torch.utils.data.Subset(trainset, indicies)
 trainloader = torch.utils.data.DataLoader(subset, batch_size=batchSize, shuffle=True, num_workers=2)
+kmeansloader = torch.utils.data.DataLoader(subset, batch_size=len(trainset))
 
+class Autoencoder(nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = nn.Sequential( # like the Composition layer you built
+            nn.Conv2d(1, 16, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 7)
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, 7),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+    def encode(self, x):
+        return self.encoder(x)
 
 def make_cost_m(cm):
     s = np.max(cm)
     return (- cm + s)
 
-if __name__ == '__main__':
+
+def kmeans(x, y, reshapeSize):
     model = KMeans(n_clusters=10)
-    x = next(iter(trainloader))
-    x, y = x[0].numpy(), x[1].numpy()
-    x = np.reshape(x, (batchSize, 28*28))
+    x = np.reshape(x, (subsetSize, reshapeSize))
     predicted_labels = model.fit_predict(x)
 
 
     cm = confusion_matrix(y, predicted_labels)
     indexes = linear_sum_assignment(make_cost_m(cm))
-    indexes = np.asarray(indexes)
-    indexes = np.transpose(indexes)
+    indexes = np.transpose(np.asarray(indexes))
     js = [e[1] for e in sorted(indexes, key=lambda x: x[0])]
     cm2 = cm[:, js]
     ax = sns.heatmap(cm2, annot=True, fmt="d", cmap="Blues")
-    #plt.show()
+    plt.show()
     accuracy = np.trace(cm2) / np.sum(cm2)
     print(f'Accuracy = {accuracy}')
+
+def kmeansfvectors():
+    def train(model, num_epochs=5, learning_rate=1e-3):
+        torch.manual_seed(42)
+        criterion = nn.MSELoss() # mean square error loss
+        optimizer = torch.optim.Adam(model.parameters(),
+                                     lr=learning_rate, 
+                                     weight_decay=1e-5) # <--
+        outputs = []
+        for epoch in range(num_epochs):
+            for data in trainloader:
+                img, _ = data
+                recon = model(img)
+                loss = criterion(recon, img)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+            print('Epoch:{}, Loss:{:.4f}'.format(epoch+1, float(loss)))
+            outputs.append((epoch, img, recon),)
+        return outputs
+
+    model = Autoencoder()
+    max_epochs = 20
+    train(model, num_epochs=max_epochs)
+
+    x = next(iter(kmeansloader))
+    x, y = x[0].numpy(), x[1].numpy()
+    #x = np.reshape(x, (subsetSize, 28*28))
+    x = torch.tensor(x)
+    out = model.encode(x)
+    #print(out)
+
+    kmeans(out.detach().numpy(), y, 64)
+
+    a=5
+
+if __name__ == '__main__':
+    #x = next(iter(kmeansloader))
+    #x, y = x[0].numpy(), x[1].numpy()
+    #kmeans(x, y, 28*28)
+    kmeansfvectors()
 
